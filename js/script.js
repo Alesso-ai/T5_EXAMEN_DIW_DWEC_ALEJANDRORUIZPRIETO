77e74d5fb7d8eb16ef1cb2c3f11d2fd25a2e953b
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2024-01-17 10:02:48 +0100

    Script casi terminado

--- js/script.js
+++ js/script.js
@@ -17,14 +17,43 @@ const validar = (e) => {
   e.preventDefault();
   mensajesErrores = [];
 
-  if (nombre.value.trim().length === 0) {
-    mensajesErrores.push("El nombre no puede estar vacio");
-  }
-  if (correo.value.trim().length === 0) {
-    mensajesErrores.push("El correo no puede estar vacio");
-  }
+  nombre.value.trim().length === 0 &&
+    mensajesErrores.push("El nombre es un campo obligatorio");
+  //Validar el correo electronico
+  !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/.test(correo.value.trim())
+    ? mensajesErrores.push("El correo electrónico no es válido")
+    : null;
 
-  if (mensaje.value.trim().length === 0) {
-    mensajesErrores.push("El mensaje no puede estar vacio");
+  //Validar el mensaje
+  mensaje.value.trim().length < 10 &&
+    mensajesErrores.push("Mensaje demasiado corto");
+  //Validar el nombre
+  !/^[a-zA-Z0-9]*$/.test(nombre.value.trim()) &&
+    mensajesErrores.push("El nombre no tiene caracteres válidos");
+  //Validar email
+  !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(
+    correo.value.trim()
+  ) &&
+    mensajesErrores.push(
+      "Introduce una dirección de correo electrónico válida"
+    );
+
+  //ENVIAR O MOSTRAR MENSAJES DE ERROR
+
+  if (
+    mensajesErrores.length === 0 &&
+    confirm("¿Estás seguro de enviar el formulario?")
+  ) {
+    formulario.submit();
+  } else if (mensajesErrores.length > 0) {
+    errores.textContent = "";
+    console.log(mensajesErrores);
+    mensajesErrores.forEach(function (mensaje) {
+      const miLi = document.createElement("li");
+      miLi.textContent = mensaje;
+      errores.appendChild(miLi);
+    });
   }
 };
+
+formulario.addEventListener("submit", validar);
